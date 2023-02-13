437104b11a701edbd876385e999ad0e5550cb23d
Raul Garcia <rgarciat94@gmail.com>
2023-02-13 13:41:35 +0100

    Error ocasionado por
    //@NamedQuery(name="TarjetasBancaria.findAll", query="SELECT t FROM TarjetasBancaria t")
    Se comenta esta línea y se quita el error

--- src/main/java/com/edix/grupo/tienda/full/stack/java/entitybeans/TarjetaBancaria.java
+++ src/main/java/com/edix/grupo/tienda/full/stack/java/entitybeans/TarjetaBancaria.java
@@ -11,7 +11,7 @@ import java.util.Date;
  */
 @Entity
 @Table(name="tarjetas_bancarias")
-@NamedQuery(name="TarjetasBancaria.findAll", query="SELECT t FROM TarjetasBancaria t")
+//@NamedQuery(name="TarjetasBancaria.findAll", query="SELECT t FROM TarjetasBancaria t")
 public class TarjetaBancaria implements Serializable {
 	private static final long serialVersionUID = 1L;
 
