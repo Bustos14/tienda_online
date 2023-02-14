06a2a185567a0c6a517e57ec9a354afbf051d4a1
Raul Garcia <rgarciat94@gmail.com>
2023-02-14 11:03:42 +0100

    Se añade controlador para direcciones

---
+++ src/main/java/com/edix/grupo/tienda/full/stack/java/controllers/DireccionController.java
@@ -0,0 +1,10 @@
+package com.edix.grupo.tienda.full.stack.java.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/direccion")
+public class DireccionController {
+
+}
--- src/main/java/com/edix/grupo/tienda/full/stack/java/controllers/TarjetaController.java
+++ src/main/java/com/edix/grupo/tienda/full/stack/java/controllers/TarjetaController.java
@@ -8,15 +8,12 @@ import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.WebDataBinder;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.edix.grupo.tienda.full.stack.java.dao.TarjetaDaoImpl;
