18d6e145e48a638a673666b97f8ba9d41d538d6a
Raul Garcia <rgarciat94@gmail.com>
2023-02-17 17:41:37 +0100

    Se añade @Get para eliminar usuarios

--- src/main/java/com/edix/grupo/tienda/full/stack/java/controllers/UsuarioController.java
+++ src/main/java/com/edix/grupo/tienda/full/stack/java/controllers/UsuarioController.java
@@ -1,12 +1,17 @@
 package com.edix.grupo.tienda.full.stack.java.controllers;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.edix.grupo.tienda.full.stack.java.dao.UsuarioDaoImpl;
+import com.edix.grupo.tienda.full.stack.java.entitybeans.Usuario;
 
 @Controller
 @RequestMapping("/usuario")
@@ -21,5 +26,17 @@ public class UsuarioController {
 		model.addAttribute("todosUsuarios", udao.todos());
 		
 		return "usuarios";
+	}
+	
+	@GetMapping("/eliminar/{id}")
+	public String eliminarUsuario(Authentication auth, HttpSession sesion,Model model,@PathVariable("id") String username) {
+		
+		for(Usuario ele : udao.todos()) {
+			if(ele.getUsername().equals(username)) {
+				udao.eliminarUsuario(username);
+			}
+		}
+		
+		return "redirect:/usuario/usuarios";
 	}
 }
