7ec193a8a4df107b2a26a2c3c014a755c0281600
Raul Garcia <rgarciat94@gmail.com>
2023-02-21 13:29:07 +0100

    arreg

--- src/main/java/com/edix/grupo/tienda/full/stack/java/dao/PedidoDao.java
+++ src/main/java/com/edix/grupo/tienda/full/stack/java/dao/PedidoDao.java
@@ -10,5 +10,7 @@ public interface PedidoDao {
 	Pedido obtenerCarrito(String username);
 	boolean guardarPedido(Pedido pedido);
 	boolean efectuarCompra (Pedido pedido);
+	List<Pedido> pedidoRealizado(String username);
+	boolean elminarPedido(int idPedido);
 
 }
