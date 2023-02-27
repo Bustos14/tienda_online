21f7994e605dc90d3dd8009c27c0e5af203f5a0d
Raul Garcia <rgarciat94@gmail.com>
2023-02-27 18:59:40 +0100

    mod

--- src/main/java/com/edix/grupo/tienda/full/stack/java/restcontroller/UsuarioRestController.java
+++ src/main/java/com/edix/grupo/tienda/full/stack/java/restcontroller/UsuarioRestController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.edix.grupo.tienda.full.stack.java.dao.DireccionDaoImpl;
 import com.edix.grupo.tienda.full.stack.java.dao.UsuarioDaoImpl;
+import com.edix.grupo.tienda.full.stack.java.entitybeans.Direccione;
 import com.edix.grupo.tienda.full.stack.java.entitybeans.Usuario;
 
 //La anotación @CrossOrigins(origins=”*”), es para permitir accesos desde aplicaciones cliente web
@@ -21,29 +22,16 @@ import com.edix.grupo.tienda.full.stack.java.entitybeans.Usuario;
 @RequestMapping("/api/usuario")
 public class UsuarioRestController {
 
-	@Autowired
-	private UsuarioDaoImpl udao;
 	
 	@Autowired
 	private DireccionDaoImpl ddao;
+	
 	@GetMapping("/usuarios/{localidad}")
-	public ResponseEntity<List<String>> findByLocalidad(@PathVariable("localidad") String localidad) {
+	public List<Direccione> findByLocalidad(@PathVariable("localidad") String localidad) {
 		
-	    List<Usuario> milista = udao.buscarPorLocalidad(localidad);
+		
+	    return ddao.findByLocalidad(localidad); 
 	    
-	    if (milista == null || milista.isEmpty()) {
-	        return ResponseEntity.notFound().build();
-	    }
-	    
-	    List<String> aMostrar = new ArrayList<>();
-
-	    for (Usuario user : milista) {
-	        String cadenaGuardar = "Nombre usuario : " + user.getNombre() + " || con apellidos : " + user.getApellidos() +
-	                " || con fecha de nacimiento : " + user.getFechaNacimiento() + " || y su username es : " + user.getUsername();
-	        aMostrar.add(cadenaGuardar);
-	    }
-
-	    return ResponseEntity.ok(aMostrar);
 	}
 
 }
